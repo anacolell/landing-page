43160c7e5ce9be95c2ef3b76c61b2b66dd33fb3c
ana colell <anacolell8@gmail.com>
2021-08-18 19:49:41 +0300

    Added cards section

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,8 @@ export default function Card({ src, text, category }) {
   return (
     <>
       <li className="card">
-        <div className="card-img-wrapper" category={category}>
-          <img src={src} alt="travel image" className="card-img" />
-        </div>
+        <p className="card-category">{category}</p>
+        <img src={src} alt="travel image" className="card-img" />
         <div className="card-info">
           <h5 className="card-text">{text}</h5>
         </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,9 +3,9 @@ import Card from "./Card";
 
 export default function Cards() {
   return (
-    <div className="cards">
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <div className="cards-container">
+    <>
+      <div className="cards">
+        <h1 className="cards-page-title">Lorem ipsum dolor sit amet.</h1>
         <div className="cards-wrapper">
           <ul className="card-items">
             <Card
@@ -14,9 +14,34 @@ export default function Cards() {
               category="Culture"
               path="/"
             />
+            <Card
+              src="images/scuba diving.jpeg"
+              text="Ship wreck diving"
+              category="Adventure"
+              path="/"
+            />
+            <Card
+              src="images/wine.jpeg"
+              text="Wine tasting in Aigio"
+              category="Gastronomy"
+              path="/"
+            />
+            <Card
+              src="images/greekfood.jpeg"
+              text="Crete food tour"
+              category="Gastronomy"
+              path="/"
+            />
+            <Card
+              src="images/beach.jpg"
+              text="5-day beach trip to Kefalonia"
+              category="Relax"
+              path="/"
+            />
+            <h4>See more</h4>
           </ul>
         </div>
       </div>
-    </div>
+    </>
   );
 }
