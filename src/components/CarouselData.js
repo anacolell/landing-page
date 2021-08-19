c92e4a052a2d71a4b55dc54df35602c2c756c98a
ana colell <anacolell8@gmail.com>
2021-08-19 19:59:37 +0300

    Fixed bug in navbar

--- src/components/CarouselData.js
+++ src/components/CarouselData.js
@@ -16,16 +16,4 @@ export const slides = [
       "https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGJlYWNofGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, distinctio. Fugiat impedit dicta voluptates officiis eos perferendis assumenda sequi ad blanditiis!",
   },
-  {
-    id: 4,
-    image:
-      "https://images.unsplash.com/photo-1508313157893-34fe6176c189?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGJlYWNofGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore velit, quaerat amet illo labore velit, quaerat amet illo Labore velit, quaerat amet illo  Labore velit, quaerat amet illo  Labore velit, quaerat amet illo  Labore velit, quaerat amet illo  Labore velit, quaerat amet illo! ",
-  },
-  {
-    id: 5,
-    image:
-      "https://images.unsplash.com/photo-1515404929826-76fff9fef6fe?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fGJlYWNofGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, distinctio, quaerat amet illo dignissimos. Labore velit, quaerat amet illo.",
-  },
 ];
